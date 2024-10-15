d40a7ab003dda1a670633a26c3a5e4eaccd268fa
Liberbell <liberbell@gmail.com>
2024-10-16 08:52:05 +0900

    myscripts9

--- project11/handson_02/myscripts.js
+++ project11/handson_02/myscripts.js
@@ -21,3 +21,10 @@
 var ham = 10;
 var cheese = 10;
 
+var report = "blank";
+
+if (ham >= 10 && cheese >= 10) {
+    report = "Strong sales of both ham and cheese"
+}else if (ham === 0 && cheese === 0) {
+    
+}
