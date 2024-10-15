c06852b587accef54b4141ae9054c9412838d8fc
Liberbell <liberbell@gmail.com>
2024-10-16 08:46:23 +0900

    myscripts7

--- project11/handson_02/myscripts.js
+++ project11/handson_02/myscripts.js
@@ -3,12 +3,17 @@
 // alert("That is: "+kg+" kilograms");
 // console.log("Conversion Complete!");
 var hot = false
-var temp = 100
+var temp = 50
 
 if (temp > 80) {
     hot = true
     console.log("Hot outside!");
+}else if (temp <= 80 && temp >= 50) {
+    console.log("Average temp outside");
+    
+}else if (temp < 50 && temp >= 32) {
+    console.log("It`s pretty cold out");
 }else{
-    console.log("It`s not very hot today");
+    console.log("It is very cold out.");
     
 }
