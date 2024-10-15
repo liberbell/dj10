b466c62ea9401a18ad977f3a037e9db4663de85b
Liberbell <liberbell@gmail.com>
2024-10-16 08:37:43 +0900

    myscripts3

--- project11/handson_02/myscripts.js
+++ project11/handson_02/myscripts.js
@@ -3,9 +3,11 @@
 // alert("That is: "+kg+" kilograms");
 // console.log("Conversion Complete!");
 var hot = false
-var temp = 60
+var temp = 100
 
 if (true > 80) {
+    hot = true
     console.log("Temperature is greater than 80");
     
 }
+console.log();
