527f2a7a23e2c95a738a60555de24597c9da5b05
Liberbell <liberbell@gmail.com>
2024-10-16 08:50:14 +0900

    myscripts8

--- project11/handson_02/myscripts.js
+++ project11/handson_02/myscripts.js
@@ -2,18 +2,22 @@
 // var kg = lbs * 0.454;
 // alert("That is: "+kg+" kilograms");
 // console.log("Conversion Complete!");
-var hot = false
-var temp = 50
+// var hot = false
+// var temp = 31
 
-if (temp > 80) {
-    hot = true
-    console.log("Hot outside!");
-}else if (temp <= 80 && temp >= 50) {
-    console.log("Average temp outside");
+// if (temp > 80) {
+//     hot = true
+//     console.log("Hot outside!");
+// }else if (temp <= 80 && temp >= 50) {
+//     console.log("Average temp outside");
     
-}else if (temp < 50 && temp >= 32) {
-    console.log("It`s pretty cold out");
-}else{
-    console.log("It is very cold out.");
+// }else if (temp < 50 && temp >= 32) {
+//     console.log("It`s pretty cold out");
+// }else{
+//     console.log("It is very cold out.");
     
-}
+// }
+
+var ham = 10;
+var cheese = 10;
+
