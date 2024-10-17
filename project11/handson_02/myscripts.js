279f296ee4024f459112c8f8dd5121ccdd064332
Liberbell <liberbell@gmail.com>
2024-10-17 14:25:50 +0900

    myscripts10

--- project11/handson_02/myscripts.js
+++ project11/handson_02/myscripts.js
@@ -58,9 +58,15 @@
     
 // }
 
-var word = "ABCDEFGHIJK"
+// var word = "ABCDEFGHIJK"
 
-for (var i = 0; i < word.length; i++) {
-    console.log(word[i]);
+// for (var i = 0; i < word.length; i++) {
+//     console.log(word[i]);
     
+// }
+
+var word = "ABABABAB";
+
+for (var i = 0; i < word.length; i = i+2) {
+    console.log(word[i]);
 }
