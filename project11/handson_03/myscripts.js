0409292f8bd5b1175a8499e026386a069fa5b2a8
Liberbell <liberbell@gmail.com>
2024-10-18 08:11:06 +0900

    myscripts1

--- project11/handson_03/myscripts.js
+++ project11/handson_03/myscripts.js
@@ -1,11 +1,20 @@
-var x = 0;
+// var x = 0;
 
-while (x < 5) {
-    console.log("Hello");
-    x++;
-}
+// while (x < 5) {
+//     console.log("Hello");
+//     x++;
+// }
 
-for (var i = 0; i < 5; i++) {
-    console.log("Hello with For loop");
+// for (var i = 0; i < 5; i++) {
+//     console.log("Hello with For loop");
     
+// }
+
+var num = 1;
+while (num < 25) {
+    if (num%2 !== 0) {
+        console.log(num);
+        
+    }
+    num++
 }
