af9c9cbd06e47cfe6ab5d521562c6d7ae2930269
Liberbell <liberbell@gmail.com>
2024-10-19 14:21:32 +0900

    myscripts2

--- project11/handson_04/myscripts.js
+++ project11/handson_04/myscripts.js
@@ -1,6 +1,7 @@
 var firstName = prompt("First Name please:")
 var lastName = prompt("Last Name please: ")
 var Age = prompt("How old are you?")
+var height = prompt("What is your height?")
 
 var nameCond = null;
 var ageCond = null;
