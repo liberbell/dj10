2aecef646dc0f6ed2b9410dc65b2542b4257bd4f
Liberbell <liberbell@gmail.com>
2024-10-20 13:53:30 +0900

    myscripts1

--- project11/handson_06/myscripts.js
+++ project11/handson_06/myscripts.js
@@ -5,3 +5,15 @@ function sleepIn(weekday, vacation) {
 function monkeyTrouble(aSmile, bSmile) {
     
 }
+
+function stringTimes(str, n) {
+    
+}
+
+function luckySum(a, b, c) {
+    
+}
+
+function caught_speeding(speed, is_birthday) {
+    
+}
