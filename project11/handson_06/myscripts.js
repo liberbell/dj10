6e0355d5f518f7b41fa28c26fb63431a9f84b3f4
Liberbell <liberbell@gmail.com>
2024-10-21 08:22:24 +0900

    myscripts4

--- project11/handson_06/myscripts.js
+++ project11/handson_06/myscripts.js
@@ -18,7 +18,13 @@ function stringTimes(str, n) {
 
 function luckySum(a, b, c) {
     if (a == 13) {
-        return 0
+        return 0;
+    }
+    if (b == 13) {
+        return a;
+    }
+    if (c == 13) {
+        return a + b;
     }
 }
 
