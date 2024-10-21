82e1ddd1c9f792301b341f349f6bd549c5e617fd
Liberbell <liberbell@gmail.com>
2024-10-22 08:26:23 +0900

    myscripts2

--- project11/handson_07/myscripts.js
+++ project11/handson_07/myscripts.js
@@ -1,5 +1,12 @@
 var roster = []
 
 function addNew() {
-    var newName = prompt("What name would you like to add?")
+    var newName = prompt("What name would you like to add?");
+    roster.push(newName);
+}
+
+function remove() {
+    var remName = prompt("What name would you like to remove?");
+    var index = roster.indexOf(remName);
+    roster.splice(index, 1);
 }
