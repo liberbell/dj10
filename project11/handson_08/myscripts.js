7889fb9b6b3fb496221c8843ca2ca98debfa1544
Liberbell <liberbell@gmail.com>
2024-10-22 08:49:04 +0900

    myscripts4

--- project11/handson_08/myscripts.js
+++ project11/handson_08/myscripts.js
@@ -16,3 +16,13 @@ var employee = {
         alert("Name is"+this.name+", Job is"+this.job+", Age is"+this.age)
     }
 }
+
+var employee = {
+    name: "james Bond",
+    job: "spy",
+    age: 48,
+    lastname: function() {
+        console.log(this.name.split(" "));
+        
+    }
+}
