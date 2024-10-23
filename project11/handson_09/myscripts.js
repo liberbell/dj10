0d520c68a73482a4e94d6ea28cb467ec441321e0
Liberbell <liberbell@gmail.com>
2024-10-24 08:04:49 +0900

    myscripts4

--- project11/handson_09/myscripts.js
+++ project11/handson_09/myscripts.js
@@ -4,8 +4,8 @@ var squares = document.querySelectorAll('td');
 
 function clearBoard() {
     for (var i = 0; i < squares.length; i++) {
-        squares[i].textContent = "";
+        squares[i].textContent = '';
         
     }
 }
-restart.addEventListener("click", clearBoard);
+restart.addEventListener('click', clearBoard);
