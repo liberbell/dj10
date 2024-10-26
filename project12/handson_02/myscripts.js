440fe8fc326ee4686e3e32d42e8026618c665e1a
Liberbell <liberbell@gmail.com>
2024-10-26 10:52:31 +0900

    myscripts7

--- project12/handson_02/myscripts.js
+++ project12/handson_02/myscripts.js
@@ -1,16 +1,20 @@
-$('h1').click(function(){
-    $(this).text("I was changed!")
-})
+// $('h1').click(function(){
+//     $(this).text("I was changed!")
+// })
 
-$("li").click(function() {
-    console.log("any li was clicked!");
+// $("li").click(function() {
+//     console.log("any li was clicked!");
     
-})
+// })
 
-$("input").eq(0).keypress(function(event) {
-    $("h3").toggleClass("turnBlue");
-    console.log(event);
-    if (event.which === 13) {
-        $("h3").toggleClass("turnRed");
-    }
+// $("input").eq(0).keypress(function(event) {
+//     $("h3").toggleClass("turnBlue");
+//     console.log(event);
+//     if (event.which === 13) {
+//         $("h3").toggleClass("turnRed");
+//     }
+// })
+
+$("h1").on("dblclick", function() {
+    $(this).toggleClass("turnBlue")
 })
