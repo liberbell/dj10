0d6589f89ac58b27463eafe33c0167769558b9a5
Liberbell <liberbell@gmail.com>
2024-10-26 10:50:39 +0900

    myscripts6

--- project12/handson_02/myscripts.js
+++ project12/handson_02/myscripts.js
@@ -7,6 +7,10 @@ $("li").click(function() {
     
 })
 
-$("input").eq(0).keypress(function() {
+$("input").eq(0).keypress(function(event) {
     $("h3").toggleClass("turnBlue");
+    console.log(event);
+    if (event.which === 13) {
+        $("h3").toggleClass("turnRed");
+    }
 })
