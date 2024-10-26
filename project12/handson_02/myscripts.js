e5db383f6178bcf0c79445e017f1f01262cec4ec
Liberbell <liberbell@gmail.com>
2024-10-26 10:41:08 +0900

    myscripts5

--- project12/handson_02/myscripts.js
+++ project12/handson_02/myscripts.js
@@ -1,8 +1,12 @@
 $('h1').click(function(){
-    $(this).textContent("I was changed!")
+    $(this).text("I was changed!")
 })
 
 $("li").click(function() {
     console.log("any li was clicked!");
     
+})
+
+$("input").eq(0).keypress(function() {
+    $("h3").toggleClass("turnBlue");
 })
