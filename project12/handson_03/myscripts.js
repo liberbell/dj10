70cf2261200d1714de9cc5a50abe5e4c82e1174c
Liberbell <liberbell@gmail.com>
2024-10-27 18:49:19 +0900

    myscripts3

--- project12/handson_03/myscripts.js
+++ project12/handson_03/myscripts.js
@@ -2,7 +2,13 @@ var player1 = prompt("Player One: Enter your name, you will be Blue");
 var player1Color = "rgb(86, 151, 255)";
 
 var player2 = prompt("Player Two: Enter your name, you will be Red");
-var player1Color = "rgb(237, 45, 73)";
+var player2Color = "rgb(237, 45, 73)";
 
 var game_on = true;
 var table = $("table tr");
+
+function reportWin(rowNum, colNum) {
+    console.log("You won starting at this row, col");
+    console.log(rowNum);
+    console.log(colNum);
+}
