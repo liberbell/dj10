7d088cefce1d73c17cbd4a7931a8084e87ba5cdd
Liberbell <liberbell@gmail.com>
2024-10-29 11:48:45 +0900

    myscripts11

--- project12/handson_03/myscripts.js
+++ project12/handson_03/myscripts.js
@@ -63,7 +63,7 @@ function verticalWinCheck() {
     }
 }
 
-function digonalWinCheck() {
+function diagonalWinCheck() {
     for (var col = 0; col < 5; col++){ 
         for (var row = 0; row < 7; row++){
             if (colorMatchCheck(returnColor(row, col), returnColor(row + 1, col + 1), returnColor(row + 2, col + 2))) {
@@ -90,10 +90,10 @@ $("h3").text(player1+" it is your turn, pick a column to drop in!");
 $(".border button").on("click", function() {
     
     var col = $(this).closest("td").index();
-    var bottomAvail = checkBottom(col);
+    var bottomAvail = checkBotton(col);
     changeColor(bottomAvail, col, currentColor);
 
-    if (horizontalWinCheck() || verticalWinCheck() || digonalWinCheck()) {
+    if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
         $("h1").text(currentName + " You have won!");
         $("h3").fadeout("fast");
         $("h2").fadeout("fast");
