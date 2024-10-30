e0112f68dc4a81d863bc4ea3c69168ee1eb727a0
Liberbell <liberbell@gmail.com>
2024-10-31 08:55:59 +0900

    exercise9

--- project13/handson_01/exercise.py
+++ project13/handson_01/exercise.py
@@ -15,6 +15,10 @@ d1 = {"simple_key": "hello"}
 d2 = {"k1": {"k2": "hello"}}
 d3 = {"k1": [{"nest_key": ["this is deep", ["hello"]]}]}
 
+print(d1["simple_key"])
+print(d2["k1"]["k2"])
+print(d3["k1"][0]["nest_key"][1])
+
 mylist = [1,1,1,2,2,2,2,3,3,3]
 converted = set(mylist)
 print(converted)
