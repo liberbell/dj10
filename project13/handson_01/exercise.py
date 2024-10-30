ae6360f19c45aebafe0387bd24b5bc86dd31f4ce
Liberbell <liberbell@gmail.com>
2024-10-31 08:47:07 +0900

    exercise5

--- project13/handson_01/exercise.py
+++ project13/handson_01/exercise.py
@@ -10,3 +10,7 @@ l = [3, 7, [1, 4, "hello"]]
 print(l)
 l[2][2] = "goodbye"
 print(l)
+
+d1 = {"simple_key": "hello"}
+d2 = {"k1": {"k2": "hello"}}
+d3 = {"k1": [{"nest_key": ["this is deep", ["hello"]]}]}
