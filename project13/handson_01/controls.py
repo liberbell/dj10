518e6bcdb8074c91f803a271da1a87e5f73ea34e
Liberbell <liberbell@gmail.com>
2024-11-01 08:57:34 +0900

    controls9

--- project13/handson_01/controls.py
+++ project13/handson_01/controls.py
@@ -17,5 +17,6 @@ d = {"james":1, "Q":2, "M": 3}
 #     print(d[k])
 
 mypairs = [(1, 2), (3, 4), (5, 6)]
-for item in mypairs:
-    print(item)
+for (tup1, tup2) in mypairs:
+    print(tup1)
+    print(tup2)
