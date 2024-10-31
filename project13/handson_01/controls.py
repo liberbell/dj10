eeec96bcf7195567c3b42f98cfcd2041509f1cbc
Liberbell <liberbell@gmail.com>
2024-11-01 08:50:19 +0900

    controls5

--- project13/handson_01/controls.py
+++ project13/handson_01/controls.py
@@ -1,6 +1,12 @@
-if 1 > 2:
-    print("First block")
-elif 3 == 3:
-    print("elif ran")
-else:
-    print("last")
+if 1 == 1:
+    if 1 > 2:
+        print("hello")
+    elif 3 == 3:
+        print("elif ran")
+    else:
+        print("last")
+
+
+seq = [1,2,3,4,5,6,7,8,9]
+for item in seq:
+    print(item)
