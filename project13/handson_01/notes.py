c873821809b218f33ec214a0ba651b901e0b869e
Liberbell <liberbell@gmail.com>
2024-11-02 10:38:27 +0900

    notes7

--- project13/handson_01/notes.py
+++ project13/handson_01/notes.py
@@ -14,7 +14,13 @@ result = hello()
 print(result)
 
 def addNum(num1, num2):
-    return num1 + num2
+    if type(num1) == type(num2)==type(10):
+        return num1 + num2
+    else:
+        return "Sorry need integers"
 
 result = addNum(10, 20)
+print(result)
+
+result = addNum("2", "3")
 print(result)
