a630ff5ef91b5922b87c43e94b3456c93a61c900
Liberbell <liberbell@gmail.com>
2024-11-02 10:32:41 +0900

    notes4

--- project13/handson_01/notes.py
+++ project13/handson_01/notes.py
@@ -4,4 +4,10 @@ def my_func(param1="default"):
     """
     print("my first python function! {}".format(param1))
 
-my_func()
+my_func("I`m james bond.")
+
+def hello():
+    # print("Hello.")
+    return "hello"
+
+hello()
