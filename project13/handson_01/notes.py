558d6b2d28ce401031a91ff4bcab8ba09c2ccc53
Liberbell <liberbell@gmail.com>
2024-11-02 10:43:32 +0900

    notes8

--- project13/handson_01/notes.py
+++ project13/handson_01/notes.py
@@ -24,3 +24,11 @@ print(result)
 
 result = addNum("2", "3")
 print(result)
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def even_bool(num):
+    return num%2 == 0
+
+evens = filter(even_bool, my_list)
+print(evens)
