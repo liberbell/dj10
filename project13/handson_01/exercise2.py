f9ef8d801751932e960fb55998e3e62b3902112e
Liberbell <liberbell@gmail.com>
2024-11-03 14:14:57 +0900

    exercise22

--- project13/handson_01/exercise2.py
+++ project13/handson_01/exercise2.py
@@ -1,4 +1,7 @@
 def arrayCheck(nums):
     for i in range(len(nums-2)):
-        if nums[i] == 1 and nums[i + 1] == 2 and nums[i + 2] == 2:
-            pass
+        if nums[i] == 1 and nums[i + 1] == 2 and nums[i + 2] == 3:
+            return True
+    return False
+
+print(arrayCheck([1, 1, 2, 3, 1]))
