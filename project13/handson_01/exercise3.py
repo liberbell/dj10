d53a80570ef0961097ad419c2211c480e2ba907d
Liberbell <liberbell@gmail.com>
2024-11-04 15:08:58 +0900

    exercise33

--- project13/handson_01/exercise3.py
+++ project13/handson_01/exercise3.py
@@ -7,5 +7,13 @@ def generate_guess():
     random.shuffle(digits)
     return digits[:3]
 
+def generate_clues(code, user_guess):
+    if user_guess == code:
+        return "Code Cracked!"
+    
+    clues = []
+    for ind, num in enumerate(user_guess):
+        if num == code[ind]:
+
 x = get_guess()
 print(x)
