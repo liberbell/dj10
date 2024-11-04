46fffa2bb15b4c5b1f247bac92560153c3f0ee0f
Liberbell <liberbell@gmail.com>
2024-11-04 14:48:42 +0900

    exercise24

--- project13/handson_01/exercise2.py
+++ project13/handson_01/exercise2.py
@@ -45,8 +45,10 @@ def no_teen_sum(a, b, c):
     
 
 def fix_teen(n):
-    if n [13, 14, 17, 18, 19]:
+    if n in [13, 14, 17, 18, 19]:
         return 0
     return n
 
-print()(no_teen_sum(1, 2, 3))
+print(no_teen_sum(1, 2, 3))
+print(no_teen_sum(2, 13, 1))
+print(no_teen_sum(2, 1, 14))
