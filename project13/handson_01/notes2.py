150a6ee8aad0682224176d4fd03dacddbdeb78c4
Liberbell <liberbell@gmail.com>
2024-11-05 08:27:03 +0900

    notes23

--- project13/handson_01/notes2.py
+++ project13/handson_01/notes2.py
@@ -4,7 +4,13 @@ def my_func():
     x = 50
     return x
 
-print(x)
-print(my_func())
-my_func()
-print()
+# print(x)
+# print(my_func())
+# my_func()
+# print(x)
+
+# lambda x: x ** 2
+name = "This is a global name"
+
+def greet():
+    name = "summy"
