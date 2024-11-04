c27b0ebcb146edcf258f8516c85d503aaabfef34
Liberbell <liberbell@gmail.com>
2024-11-04 15:13:43 +0900

    exercise35

--- project13/handson_01/exercise3.py
+++ project13/handson_01/exercise3.py
@@ -17,6 +17,10 @@ def generate_clues(code, user_guess):
             clues.append("match")
         elif num in code:
             clues.append("close")
+    if clues == []:
+        return ["Nope"]
+    else:
+        return clues
 
 x = get_guess()
 print(x)
