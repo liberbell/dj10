909e2f197062636ff20faee3595184af75d32346
Liberbell <liberbell@gmail.com>
2024-11-09 12:55:49 +0900

    oop_project1

--- project13/handson_02/oop_project.py
+++ project13/handson_02/oop_project.py
@@ -3,8 +3,12 @@ from random import shuffle
 SUITE = "H D S C".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K".split()
 
+mycards = [(s, r) for s in SUITE for r in RANKS]
 class Deck:
-    pass
+    
+    def __init__(self):
+        print("Creating new ordered deck...")
+        self.
 
 class Hand:
     pass
