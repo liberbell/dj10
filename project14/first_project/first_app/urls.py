948a8147c704e62e97627bdb7d987185be5b5344
Liberbell <liberbell@gmail.com>
2024-11-15 08:46:21 +0900

    urls1

--- project14/first_project/first_app/urls.py
+++ project14/first_project/first_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('index', views.index, name="firstapp_index"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
