10a4572f8da93d75d25e8ee7f607ceb9cbbb00f1
Liberbell <liberbell@gmail.com>
2024-11-16 10:30:33 +0900

    views1

--- project14/first_project/first_app/views.py
+++ project14/first_project/first_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(request):
     my_dict = {"insert_me": "Hello I am comming from first_app/index.html!"}
     my_dict2 = {"insert_content": "Hello I am from first app!"}
-    return render(request, "first_app/index.html", context=my_dict)
+    return render(request, "first_app/index.html", context=my_dict2)
