0a09660375c28441368484d6261f776f3ee7ff91
Liberbell <liberbell@gmail.com>
2024-11-16 10:42:04 +0900

    models2

--- project14/first_project/first_app/models.py
+++ project14/first_project/first_app/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Topic(models.Model):
     top_name = models.CharField(max_length=254, unique=True)
+
+    def __str__(self):
+        return self.top_name
+    
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=254, unique=True)
