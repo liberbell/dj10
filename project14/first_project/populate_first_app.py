ecc609e50ab41456418d2fb650d2b7c9b35ca5d1
Liberbell <liberbell@gmail.com>
2024-11-17 08:30:22 +0900

    populate_first_app2

--- project14/first_project/populate_first_app.py
+++ project14/first_project/populate_first_app.py
@@ -1,4 +1,11 @@
 import os
 import django
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
 
 os.environ.setdefault("DJANGO_SETTING_MODULE", "first_project.settings")
+django.setup()
+
+fakegen = Faker()
+topics = ["Search", "Social", "Marketplace"]
