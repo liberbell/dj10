f1a6df5644cca0708840ffbdcfa8425aedcdf926
Liberbell <liberbell@gmail.com>
2024-11-17 08:45:35 +0900

    populate_first_app10

--- project14/first_project/populate_first_app.py
+++ project14/first_project/populate_first_app.py
@@ -5,7 +5,7 @@ from first_app.models import AccessRecord, Webpage, Topic
 from faker import Faker
 
 # os.environ.setdefault("DJANGO_SETTING_MODULE", "first_project.settings")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_project.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "first_project.settings"
 django.setup()
 
 fakegen = Faker()
