41bc58b405330a6a7040d166adc6820742ed07c3
Liberbell <liberbell@gmail.com>
2024-11-18 08:23:16 +0900

    views2

--- project14/first_project/first_app/views.py
+++ project14/first_project/first_app/views.py
@@ -6,6 +6,6 @@ from first_app.models import AccessRecord, Topic, Webpage
 def index(request):
     webpages_list = AccessRecord.objects.order_by("date")
     date_dict = {"access_record": webpages_list}
-    my_dict = {"insert_me": "Hello I am comming from first_app/index.html!"}
-    my_dict2 = {"insert_content": "Hello I am from first app!"}
-    return render(request, "first_app/index.html", context=my_dict2)
+    # my_dict = {"insert_me": "Hello I am comming from first_app/index.html!"}
+    # my_dict2 = {"insert_content": "Hello I am from first app!"}
+    return render(request, "first_app/index.html", context=date_dict)
