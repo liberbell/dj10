f471edc3b56d55b0e2b309142983dbe7c8af8166
Liberbell <liberbell@gmail.com>
2024-11-18 08:32:18 +0900

    views3

--- project14/first_project/first_app/views.py
+++ project14/first_project/first_app/views.py
@@ -5,7 +5,7 @@ from first_app.models import AccessRecord, Topic, Webpage
 # Create your views here.
 def index(request):
     webpages_list = AccessRecord.objects.order_by("date")
-    date_dict = {"access_record": webpages_list}
+    date_dict = {"access_records": webpages_list}
     # my_dict = {"insert_me": "Hello I am comming from first_app/index.html!"}
     # my_dict2 = {"insert_content": "Hello I am from first app!"}
     return render(request, "first_app/index.html", context=date_dict)
