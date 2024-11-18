8b73dbbcfe497cf0c120b2f8c7fa16f2bda0f2a4
Liberbell <liberbell@gmail.com>
2024-11-19 08:45:52 +0900

    models2

--- project14/ProThree/appTwo/models.py
+++ project14/ProThree/appTwo/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Users(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    Email = models.EmailField(max_length=1024)
+    email = models.EmailField(max_length=1024, unique=True)
