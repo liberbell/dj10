300fd5093fc746e834566ea49c60484198f3de04
Liberbell <liberbell@gmail.com>
2024-11-20 08:21:19 +0900

    views2

--- project14/ProThree/appTwo/views.py
+++ project14/ProThree/appTwo/views.py
@@ -4,3 +4,7 @@ from appTwo.models import Users
 # Create your views here.
 def index(request):
     return render(request, "appthree/index.html")
+
+def users(request):
+    
+    user_list = Users.objects.order_by("first_name")
