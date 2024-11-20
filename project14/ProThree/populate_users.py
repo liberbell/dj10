c6d269160418b682b61567b2d576bbbdf2412453
Liberbell <liberbell@gmail.com>
2024-11-21 08:40:52 +0900

    populate_users4

--- project14/ProThree/populate_users.py
+++ project14/ProThree/populate_users.py
@@ -15,3 +15,13 @@ def populate(N=5):
         fake_name = fakegen.name().split()
         fake_first_name = fake_name[0]
         fake_last_name = fake_name[1]
+        fake_email = fakegen.email()
+
+        user = User.objects.get_or_create(first_name=fake_first_name,
+                                          last_name=fake_last_name,
+                                          email=fake_email)[0]
+
+if __name__ == "__main__":
+    print("populating data")
+    populate(20)
+    print("populateing data complete")
