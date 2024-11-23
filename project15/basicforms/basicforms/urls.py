2e806b15ecfe1d5216e1647b4996190ab299d98a
Liberbell <liberbell@gmail.com>
2024-11-23 11:01:40 +0900

    urls2

--- project15/basicforms/basicforms/urls.py
+++ project15/basicforms/basicforms/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from basicapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('basicapp/', include("basicapp.urls"))
+    path('basicapp/', include("basicapp.urls")),
+    path('index/', views.index, name="index"),
 ]
