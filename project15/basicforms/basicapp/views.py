8406092b352f0c812492ea890c331a537ce276ea
Liberbell <liberbell@gmail.com>
2024-11-23 10:52:59 +0900

    views2

--- project15/basicforms/basicapp/views.py
+++ project15/basicforms/basicapp/views.py
@@ -4,3 +4,7 @@ from . import forms
 # Create your views here.
 def index(request):
     return render(request, "basicapp/index.html")
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request, "basicapp/form_page.html")
