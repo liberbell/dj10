60843a7ff9d5adb8ea76f662d5839a56731221df
Liberbell <liberbell@gmail.com>
2024-11-25 08:33:24 +0900

    forms1

--- project15/basicforms/basicapp/forms.py
+++ project15/basicforms/basicapp/forms.py
@@ -4,3 +4,9 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcacher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_botcacher(self):
+        botcacher = self.cleaned_data["botcacher"]
+        if len(botcacher) > 0:
+            
