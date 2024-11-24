258d526d04f5ceb35d0e4ddd26252004e9c036fb
Liberbell <liberbell@gmail.com>
2024-11-24 17:54:28 +0900

    views4

--- project15/basicforms/basicapp/views.py
+++ project15/basicforms/basicapp/views.py
@@ -16,4 +16,4 @@ def form_name_view(request):
             print("Email:"+form.cleaned_data["email"])
             print("Text:"+form.cleaned_data["text"])
 
-    return render(request, "basicapp/form_page.html", context={"form": form})
+    return render(request, "index", context={"form": form})
