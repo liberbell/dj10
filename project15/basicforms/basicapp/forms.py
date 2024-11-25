4288cf16cace4efbb522a3e526b934c6e200277d
Liberbell <liberbell@gmail.com>
2024-11-26 08:19:55 +0900

    forms1

--- project15/basicforms/basicapp/forms.py
+++ project15/basicforms/basicapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
@@ -6,8 +7,8 @@ class FormName(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     botcacher = forms.CharField(required=False, widget=forms.HiddenInput)
 
-    def clean_botcacher(self):
-        botcacher = self.cleaned_data["botcacher"]
-        if len(botcacher) > 0:
-            raise forms.ValidationError("GOTCHA BOT!")
-        return botcacher
+    # def clean_botcacher(self):
+    #     botcacher = self.cleaned_data["botcacher"]
+    #     if len(botcacher) > 0:
+    #         raise forms.ValidationError("GOTCHA BOT!")
+    #     return botcacher
