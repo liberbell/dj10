197cf8034072709f987f26007f4d1a17208c27fb
Liberbell <liberbell@gmail.com>
2024-11-29 08:14:41 +0900

    urls1

--- project15/learning_templates/learning_templates/urls.py
+++ project15/learning_templates/learning_templates/urls.py
@@ -20,6 +20,6 @@ from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, )
-    path("basicapp/", include("basic_app.urls")),
+    path('', views.index, name="index"),
+    path("basic_app/", include("basic_app.urls")),
 ]
