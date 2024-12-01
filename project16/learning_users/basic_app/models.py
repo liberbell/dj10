4f59e36dfc1485ee0244afa0c417d3697e206d3b
Liberbell <liberbell@gmail.com>
2024-12-02 08:53:01 +0900

    models3

--- project16/learning_users/basic_app/models.py
+++ project16/learning_users/basic_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfileInfoModel(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     portfolio_site = models.URLField(max_length=200, blank=True)
     portfolio_pic = models.ImageField(upload_to="portfolio_pics", blank=Treu)
