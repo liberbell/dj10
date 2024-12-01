b27511f6e2cc7453e9f6af62d1e160dd93fc25e8
Liberbell <liberbell@gmail.com>
2024-12-01 12:29:50 +0900

    settings7

--- project16/learning_users/learning_users/settings.py
+++ project16/learning_users/learning_users/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [STATIC_URL, ]
+
+MEDIA_ROOT = MDEIA_DIR
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
