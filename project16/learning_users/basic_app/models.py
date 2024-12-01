f6c5a7f32e2e5ef149bca9a038b6bd29fa6155f4
Liberbell <liberbell@gmail.com>
2024-12-02 08:30:16 +0900

    models2

--- project16/learning_users/basic_app/models.py
+++ project16/learning_users/basic_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfileInfo(models.Model):
+class UserProfileInfoModel(models.Model):
     user = models.OneToOneField(User)
 
     portfolio = models.URLField(max_length=200, blank=True)
