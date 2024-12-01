979291f9ebd79b46cf3c931e5fa15c632dbd27da
Liberbell <liberbell@gmail.com>
2024-12-02 08:18:57 +0900

    models1

--- project16/learning_users/basic_app/models.py
+++ project16/learning_users/basic_app/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User)
+
+    portfolio = models.URLField(max_length=200, blank=True)
+    picture = models.ImageField(upload_to="portfolio_pics")
+
+    def __str__(self):
+        return self.name
