e73dc69687b5cc56cca20f37e9b9c75eb532aa29
Liberbell <liberbell@gmail.com>
2024-12-04 08:27:02 +0900

    views3

--- project16/learning_users/basic_app/views.py
+++ project16/learning_users/basic_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm, UserProfileInfo
 
 # Create your views here.
 def index(request):
     return render(request, "basic_app/index.html")
 
 def register(request):
+    registerd = False
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfo(data=request.POST)
     return render(request, "basic_app/register.html")
