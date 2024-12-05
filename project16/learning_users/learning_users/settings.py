c3595cb3aa84d3dfbb7bb35cf7e338b117009484
Liberbell <liberbell@gmail.com>
2024-12-06 08:35:40 +0900

    settings1

--- project16/learning_users/learning_users/settings.py
+++ project16/learning_users/learning_users/settings.py
@@ -139,3 +139,5 @@ DEDIA_URL = 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'basic_app/user_login'
