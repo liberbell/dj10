4a2a49ca95b255130e36ac93d76cc4291140468a
Liberbell <liberbell@gmail.com>
2024-12-06 08:45:08 +0900

    views1

--- project16/learning_users/basic_app/views.py
+++ project16/learning_users/basic_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
