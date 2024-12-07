cb1b7f6e76ab8283ceda8bc9b654a0108d450dc8
Liberbell <liberbell@gmail.com>
2024-12-07 10:40:32 +0900

    urls1

--- project16/learning_users/basic_app/urls.py
+++ project16/learning_users/basic_app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "basic_app"
 
 urlpatterns = [
     path('register', views.register, name="register"),
+    re_path('user_login/$', views.user_login, name="user_login"),
 ]
