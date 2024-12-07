efb16c8a7c009d46a85a9a8187d34a50acd2ebc3
Liberbell <liberbell@gmail.com>
2024-12-07 10:34:57 +0900

    urls1

--- project16/learning_users/learning_users/urls.py
+++ project16/learning_users/learning_users/urls.py
@@ -21,5 +21,6 @@ from basic_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
+    path('logout/', views.logout, name="logout"),
     path('basic_app/', include("basic_app.urls")),
 ]
