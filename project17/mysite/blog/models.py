30f43befcaa94d896111b3fe67d3f1158130e71c
Liberbell <liberbell@gmail.com>
2024-12-08 17:22:47 +0900

    models2

--- project17/mysite/blog/models.py
+++ project17/mysite/blog/models.py
@@ -5,3 +5,6 @@ from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=512)
+    create_date = models.DateTimeField(default=timezone.now())
