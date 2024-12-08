3a67bcbdef26dd06a8a943a79bcded8d6068d2d0
Liberbell <liberbell@gmail.com>
2024-12-08 17:21:19 +0900

    models1

--- project17/mysite/blog/models.py
+++ project17/mysite/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
