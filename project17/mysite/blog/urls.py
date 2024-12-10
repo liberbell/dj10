724f167669a46971f7d1cefa8a976796562836c4
Liberbell <liberbell@gmail.com>
2024-12-11 08:12:15 +0900

    urls1

--- project17/mysite/blog/urls.py
+++ project17/mysite/blog/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('about/', views.AboutView, name="about"),
 ]
