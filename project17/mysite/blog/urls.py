d9fbbdb491045ee8654e2d88186c46b4ec3e21eb
Liberbell <liberbell@gmail.com>
2024-12-11 08:57:14 +0900

    urls4

--- project17/mysite/blog/urls.py
+++ project17/mysite/blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name="post_list"),
     path('about/', views.AboutView.as_view(), name="about"),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name="post_detail"),
 ]
