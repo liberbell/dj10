29316c5823f39fd1cc7780d0519813cc8e6d15f7
Liberbell <liberbell@gmail.com>
2024-12-11 08:17:04 +0900

    views1

--- project17/mysite/blog/views.py
+++ project17/mysite/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .models import Post, Comment
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 class AboutView(TemplateView):
     template_name = "about.html"
+
+class PostListView(ListView):
+    model = Post
+
+    def get_query_set(self):
+        return Post.objects.filter(published_date__lte=timezone.now.order_by('-published_date'))
