40252fb9c79a1a66c53ffcd9f10036def90c3a54
Liberbell <liberbell@gmail.com>
2024-12-12 08:28:11 +0900

    vi5

--- project17/mysite/blog/views.py
+++ project17/mysite/blog/views.py
@@ -37,3 +37,6 @@ class PostUpdateView(UpdateView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy("post_list")
+
+class DraftListView(LoginRequiredMixin, ListView):
+    model = Post
