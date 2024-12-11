f5e3c6fde9b59a9cddd49f74d5d924581949e0df
Liberbell <liberbell@gmail.com>
2024-12-12 08:13:07 +0900

    urls2

--- project17/mysite/blog/urls.py
+++ project17/mysite/blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name="post_list"),
     path('about/', views.AboutView.as_view(), name="about"),
     path('post/<int:pk>', views.PostDetailView.as_view(), name="post_detail"),
-    path('post/new', views.CreatePostView.as_view(), name="post_new"),
+    path('post/new/', views.CreatePostView.as_view(), name="post_new"),
+    path('post/update/', views.PostUpdateView.as_view(), name="post_update"),
 ]
