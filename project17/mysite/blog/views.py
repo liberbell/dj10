c1e32cd13ae7efdc29a445191ea4a4816b18617c
Liberbell <liberbell@gmail.com>
2024-12-19 08:17:53 +0900

    views2

--- project17/mysite/blog/views.py
+++ project17/mysite/blog/views.py
@@ -74,7 +74,7 @@ def comment_approve(request, pk):
 
     return redirect("post_detail", pk=comment.post.pk)
 
-
+@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
