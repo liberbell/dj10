92da469006007211b74a8f5dee73177fb97eb588
Liberbell <liberbell@gmail.com>
2024-12-24 08:20:57 +0900

    views1

--- project18/simplesocial/accounts/views.py
+++ project18/simplesocial/accounts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.core import reverse_lazy
+from django.views.generic import CreateView
 
 from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    """
+    docstring
+    """
+    pass
