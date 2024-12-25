90b10e09e63628445f2c1209b66f95c55faf38d0
Liberbell <liberbell@gmail.com>
2024-12-25 13:11:38 +0900

    urls1

--- project18/simplesocial/simplesocial/urls.py
+++ project18/simplesocial/simplesocial/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Homepage.as_view(), name="home"),
+    path('accounts/', include('accounts.urls')),
 ]
