0d66a7e553119edc4bd962d09ae8fc05d9522706
Liberbell <liberbell@gmail.com>
2024-12-26 08:38:16 +0900

    urls1

--- project18/simplesocial/simplesocial/urls.py
+++ project18/simplesocial/simplesocial/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.Homepage.as_view(), name="home"),
     path('accounts/', include('accounts.urls'), namespace="accounts"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('test', views.TestPage.as_view(), name="test")
 ]
