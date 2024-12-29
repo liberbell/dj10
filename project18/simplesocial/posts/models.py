e79c251d66eb46673440511584c4f1a64df6cfa6
Liberbell <liberbell@gmail.com>
2024-12-29 13:49:20 +0900

    models2

--- project18/simplesocial/posts/models.py
+++ project18/simplesocial/posts/models.py
@@ -4,5 +4,11 @@ from django.conf import settings
 import misaka
 
 from group.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
