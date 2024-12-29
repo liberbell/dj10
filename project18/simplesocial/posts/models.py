6d8aabd3759670f43ec8e946e1686b0fb2fcd9bc
Liberbell <liberbell@gmail.com>
2024-12-29 13:53:17 +0900

    models4

--- project18/simplesocial/posts/models.py
+++ project18/simplesocial/posts/models.py
@@ -12,5 +12,11 @@ class Post(models.Model):
     user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
-    message_url = models.TextField(editable=False)
+    message_html = models.TextField(editable=False)
     group = models.ForeignKey(Group, related_name="posts", null=True, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
