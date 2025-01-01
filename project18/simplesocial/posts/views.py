4666f56a871149938a98d3a6d6dd2281910e5d48
Liberbell <liberbell@gmail.com>
2025-01-01 15:11:43 +0900

    views1

--- project18/simplesocial/posts/views.py
+++ project18/simplesocial/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from django.http import Http404
+from django.views import generic
+from braces.views import SelectRelatedMixin
 
 # Create your views here.
