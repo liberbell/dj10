926f5cd744bc5c7b05c9fd369f413fde55cca514
Liberbell <liberbell@gmail.com>
2025-01-03 12:46:49 +0900

    urls1

--- project18/simplesocial/posts/urls.py
+++ project18/simplesocial/posts/urls.py
@@ -5,5 +5,6 @@ app_name = "posts"
 
 urlpatterns = [
     path('', views.PostList.as_view(), name="all"),
-    path('new/', views.CreatePost.as_view(), name="create")
+    path('new/', views.CreatePost.as_view(), name="create"),
+    path('by/<username>/', views.UserPosts.as_view(), name="for_user"),
 ]
