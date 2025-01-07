88df1603ec5d52612be4207da582d321c58faeb1
Liberbell <liberbell@gmail.com>
2025-01-08 08:59:53 +0900

    views1

--- project18/simplesocial/posts/views.py
+++ project18/simplesocial/posts/views.py
@@ -24,7 +24,7 @@ class UserPosts(generic.ListView):
 
     def get_queryset(self):
         try:
-            self.post_user = User.objects.prefetch_related("posts").get(usernam__iexact=self.kwargs.get("username"))
+            self.post_user = User.objects.prefetch_related("posts").get(username__iexact=self.kwargs.get("username"))
         except User.DoesNotExist:
             raise Http404
         else:
