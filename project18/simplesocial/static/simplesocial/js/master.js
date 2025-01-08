d3311599ffb281125a6a1b127cd5906c25c0336e
Liberbell <liberbell@gmail.com>
2025-01-09 08:27:03 +0900

    master2

---
+++ project18/simplesocial/static/simplesocial/js/master.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("canvas"),
+    context = document.getContext("2d"),
+    canvasWidth = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth),
+    canvasHeight = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight),
+    requestAnimationFrame = window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+var persons = [],
+    numberOfFirefly = 30,
+    birthToGive = 25;
