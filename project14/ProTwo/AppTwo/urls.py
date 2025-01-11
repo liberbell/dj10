264f6d6f66aa3df92b13ac90c99de4138514890e
Liberbell <liberbell@gmail.com>
2025-01-11 10:15:03 +0900

    urls1

--- project14/ProTwo/AppTwo/urls.py
+++ project14/ProTwo/AppTwo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name="apptwo_index"),
-    path('help', views.help, name="help"),
+    path('index/', views.index, name="apptwo_index"),
+    path('help/', views.help, name="help"),
 ]
