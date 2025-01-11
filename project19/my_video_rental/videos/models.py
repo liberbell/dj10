cd0ed761e05567cf9060efac4700a1125e0116fa
Liberbell <liberbell@gmail.com>
2025-01-11 10:50:38 +0900

    models2

--- project19/my_video_rental/videos/models.py
+++ project19/my_video_rental/videos/models.py
@@ -4,4 +4,9 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     length = models.PositiveIntegerField()
-    release_year = models.PositiveSmallIntegerField()
+    release_year = models.PositiveIntegerField()
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.PositiveIntegerField()
