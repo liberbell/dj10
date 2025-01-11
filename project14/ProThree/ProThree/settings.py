b0073005bfbc8b6b509fea6e85cba38a09f182a4
Liberbell <liberbell@gmail.com>
2025-01-11 10:30:35 +0900

    settings3

--- project14/ProThree/ProThree/settings.py
+++ project14/ProThree/ProThree/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+INTERNAL_IPS = ['127.0.0.1']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
