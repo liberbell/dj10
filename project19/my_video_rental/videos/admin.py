0758380b3f83923609c452b3bdd1a03c716447b2
Liberbell <liberbell@gmail.com>
2025-01-12 13:11:59 +0900

    admin1

--- project19/my_video_rental/videos/admin.py
+++ project19/my_video_rental/videos/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']
+    
 admin.site.register(models.Customer)
 admin.site.register(models.Movie)
