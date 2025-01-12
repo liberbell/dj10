1f52194b213983a96637cd48c48d0e47c8a95bd0
Liberbell <liberbell@gmail.com>
2025-01-12 13:15:22 +0900

    admin2

--- project19/my_video_rental/videos/admin.py
+++ project19/my_video_rental/videos/admin.py
@@ -5,6 +5,8 @@ from . import models
 
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
-    
+
+    search_fields = ['title']
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
