d794e8c30d7d67feb9dd04847ff52c6c15015371
Liberbell <liberbell@gmail.com>
2025-01-14 08:51:55 +0900

    models3

--- project20/advcbv/basic_app/models.py
+++ project20/advcbv/basic_app/models.py
@@ -9,3 +9,7 @@ class School(models.Model):
     def __str__(self):
         return self.name
     
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    school = models.ForeignKey(School, related_name="students", on_delete=models.CASCADE)
