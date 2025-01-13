6c1c7b1a77ab812877da153d124f04c6e1d61d01
Liberbell <liberbell@gmail.com>
2025-01-14 08:18:05 +0900

    urls1

--- project20/advcbv/advcbv/urls.py
+++ project20/advcbv/advcbv/urls.py
@@ -21,4 +21,5 @@ from basic_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name="index"),
+    path('cbview/', views.CBView.as_view(), name="cbview"),
 ]
