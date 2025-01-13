b025834813fe1810f52e7966fad456e0d4c7e2ab
Liberbell <liberbell@gmail.com>
2025-01-14 08:47:28 +0900

    models2

--- project20/advcbv/basic_app/models.py
+++ project20/advcbv/basic_app/models.py
@@ -5,3 +5,7 @@ class School(models.Model):
     name = models.CharField(max_length=255)
     principal = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
