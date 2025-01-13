00ffd3bec37bdc1f65f2e199dad8852b1fdeb104
Liberbell <liberbell@gmail.com>
2025-01-14 08:46:51 +0900

    models1

--- project20/advcbv/basic_app/models.py
+++ project20/advcbv/basic_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    principal = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
