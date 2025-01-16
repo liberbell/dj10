ed694a1edfe1d6c13a55856a1daf803de05eaa6b
Liberbell <liberbell@gmail.com>
2025-01-17 08:29:25 +0900

    views1

--- project20/advcbv/basic_app/views.py
+++ project20/advcbv/basic_app/views.py
@@ -29,4 +29,4 @@ class SchoolListView(ListView):
 class SchoolDetailView(DetailView):
     context_object_name = "school_detail"
     model = models.School
-    template_name = 'basci_app/school_detail.html'
+    template_name = 'basic_app/school_detail.html'
